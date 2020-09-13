905b871b6d08021493136b1a3d8e62f5097e3bbc
Barry <Barry-B15@users.noreply.github.com>
2020-09-13 12:35:26 +0900

    feat: context, state, useReducer, & globalProvider

---
+++ src/context/AppReducer.js
@@ -0,0 +1,7 @@
+// a reducer is where we specify the application state changes in response to certain actions to our context
+
+export default (state, action) => {
+    switch (action.type) {
+        default: return state;
+    }
+}
---
+++ src/context/GlobalState.js
@@ -0,0 +1,32 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+//2. Initial State
+const initialState = {
+    // we only our transaction where we can do our calculations
+    transactions: [
+        { id: 1, text: 'Flower', amount: -20 },
+        { id: 2, text: 'Salary', amount: 300 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150 }
+    ]
+}
+
+//3. Create context
+// export cos we r going to use this in other components
+export const GlobalContext = createContext(initialState);
+
+//4. Provider Component  //distructuring {  children } are the Components create in App.js
+export const GlobalProvider = ({ children }) => {
+    // usereducer to access state and dispatch, We will create a separare file Appreducer for this
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return ( < GlobalContext.Provider value = {
+            {
+                transactions: state.transactions
+            }
+        } > { children } <
+        /GlobalContext.Provider>
+    );
+
+}
